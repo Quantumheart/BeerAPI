8c295b6e30a5fd9be9f7fe5cea3e85c622317d86
Cody Mumford <cmqheart@gmail.com>
2020-06-11 18:18:27 -0500

    (EF Core - add Packages) AutoMapper, EFCore, Ef.Design, and Ef.SqlServer

---
+++ BeerAPI/Data/DataContext.cs
@@ -0,0 +1,23 @@
+﻿using BeerAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BeerAPI.Data
+{
+    public class DataContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder
+                .UseSqlServer(@"Data Source=BeerDB.db;");
+        }
+
+        public DbSet<Beer> Beers { get; set; }
+        public DbSet<Brewery> Breweries { get; set; }
+        
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Beer>().ToTable("Beers");
+            modelBuilder.Entity<Brewery>().ToTable("Breweries");
+        }
+    }
+}
