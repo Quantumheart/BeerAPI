f35f12e909ac218baac9abb734bc78fe8510ea9a
Cody Mumford <cmqheart@gmail.com>
2020-06-11 17:29:58 -0500

    (Model Update) flesh out Beer.cs and Brewery.cs a bit more

--- BeerAPI/Models/Beer.cs
+++ BeerAPI/Models/Beer.cs
@@ -1,8 +1,18 @@
-﻿namespace BeerAPI.Models
+﻿using System;
+
+namespace BeerAPI.Models
 {
     public class Beer
     {
         public int Id { get; set; }
         public string BeerName { get; set; }
+        public string BeerClass { get; set; }
+        public string BeerDescription { get; set; }
+        public decimal Abv { get; set; }
+        public Brewery Brewer { get; set; }
+        public string CreatedBy { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public string ModifiedBy { get; set; }
+        public DateTime ModifiedDate { get; set; }
     }
 }
--- BeerAPI/Models/Brewery.cs
+++ BeerAPI/Models/Brewery.cs
@@ -1,11 +1,21 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 
 namespace BeerAPI.Models
 {
     public class Brewery
     {
         public int Id { get; set; }
+        public string BreweryName { get; set; }
+        public string MasterBrewer { get; set; }
+        public string Address { get; set; }
+        public string StateAbbr { get; set; }
+        public string City { get; set; }
+        public string ZipCode { get; set; }
         public List<Brewery> Beers { get; set; }
-
+        public string CreatedBy { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public string ModifiedBy { get; set; }
+        public DateTime ModifiedDate { get; set; }
     }
 }
