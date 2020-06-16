c1be981e117556a11f742630d8021a904bed7fef
Cody Mumford <cmqheart@gmail.com>
2020-06-15 19:17:48 -0500

    (CLEANUP - Add DTOs) Brewery.cs and Beer.cs

--- BeerAPI/Models/Beer.cs
+++ BeerAPI/Models/Beer.cs
@@ -25,4 +25,18 @@ namespace BeerAPI.Models
         public string ModifiedBy { get; set; }
         public DateTime ModifiedDate { get; set; }
     }
+
+    public class DtoBeer
+    {
+        public int BeerId { get; set; }
+        public string BeerName { get; set; }
+        public string BeerClass { get; set; }
+        public string BeerDescription { get; set; }
+        public decimal Abv { get; set; }
+        public Brewery Brewer { get; set; }
+        public string CreatedBy { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public string ModifiedBy { get; set; }
+        public DateTime ModifiedDate { get; set; }
+    }
 }
--- BeerAPI/Models/Brewery.cs
+++ BeerAPI/Models/Brewery.cs
@@ -30,4 +30,20 @@ namespace BeerAPI.Models
         public string ModifiedBy { get; set; }
         public DateTime ModifiedDate { get; set; }
     }
+
+    public class DtoBrewery
+    {
+        public int BreweryId { get; set; }
+        public string BreweryName { get; set; }
+        public string MasterBrewer { get; set; }
+        public string Address { get; set; }
+        public string StateAbbr { get; set; }
+        public string City { get; set; }
+        public string ZipCode { get; set; }
+        public List<Brewery> Beers { get; set; }
+        public string CreatedBy { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public string ModifiedBy { get; set; }
+        public DateTime ModifiedDate { get; set; }
+    }
 }
